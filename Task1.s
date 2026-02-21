327fb67486203995b41be3a7a60c75a93a4157c8
Kyr1e-1rving <michael.egharevba001@gmail.com>
2026-02-20 23:43:40 -0500

    Update Task1.s

--- Task1.s
+++ Task1.s
@@ -1,1 +1,21 @@
 
+.section .bss
+.global ram
+.lcomm ram, 0x100         #256 in decimal 
+
+.section .text
+.global reserve
+reserve:
+
+    movb $0xFF, ram+0x50
+    movb $0xFF, ram+0x51
+    movb $0xFF, ram+0x52
+    movb $0xFF, ram+0x53
+    movb $0xFF, ram+0x54
+    movb $0xFF, ram+0x55
+    movb $0xFF, ram+0x56
+    movb $0xFF, ram+0x57
+    movb $0xFF, ram+0x58
+    ret
+
+.section .note.GNU-stack,"",@progbits
