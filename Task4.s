ea5cac4c1ba7e196f729ab051718efd294e303f8
Kyr1e-1rving <michael.egharevba001@gmail.com>
2026-02-20 23:58:23 -0500

    Add assembly code to reserve and store value in RAM

--- Task4.s
+++ Task4.s
@@ -1,1 +1,19 @@
+.section .bss
+.global ram
+.lcomm ram, 0x100
+.section .text
+.global reserve
+reserve:
+    xor %eax, %eax         
+    mov $1, %bl            
+    mov $10, %ecx          
 
+total:
+    add %bl, %al           
+    inc %bl               
+    loop total
+    lea ram(%rip), %rdi
+    movb %al, 0x50(%rdi)
+    ret
+
+.section .note.GNU-stack,"",@progbits
