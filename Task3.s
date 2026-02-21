d52073acc737923a496b45f5f55e8c9b3835ad9f
Kyr1e-1rving <michael.egharevba001@gmail.com>
2026-02-20 23:55:21 -0500

    Implement reserve function to initialize memory

--- Task3.s
+++ Task3.s
@@ -1,1 +1,20 @@
+.section .bss
+.global ram
+.lcomm ram, 0x100
+.section .text
+.global reserve
 
+reserve:
+    lea ram(%rip), %rdi
+    add $0x50, %rdi  
+    mov $9, %ecx    
+    xor %eax, %eax  
+
+reset:
+    movb %al, (%rdi)        
+    inc %rdi
+    loop reset
+
+    ret
+
+.section .note.GNU-stack,"",@progbits
