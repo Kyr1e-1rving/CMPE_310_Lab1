dc9c32c7ca7e5347b067fda728a3cb34470233ab
Kyr1e-1rving <michael.egharevba001@gmail.com>
2026-09-12 00:00:43 -0400

    Implement fill_ram function to clear RAM locations
    Added a new function 'fill_ram' to clear specific RAM locations and defined the 'ram' variable in the .bss section.

--- Task3.s
+++ Task3.s
@@ -7,7 +7,29 @@
 reserve:
     lea ram(%rip), %rdi
     add $0x50, %rdi  
-    mov $9, %ecx    
+    mov $9, %ecx    .section .bss
+.global ram
+.lcomm ram, 256
+
+.section .text
+.global fill_ram
+fill_ram:
+    # Clear RAM locations 50H - 58H by writing 00H
+
+    lea ram(%rip), %rdi     # rdi = &ram[0]
+    add $0x50, %rdi         # rdi = &ram[0x50]
+
+    mov $9, %ecx            # 9 bytes (0x50..0x58)
+    xor %eax, %eax          # AL = 0x00
+
+loop_clear:
+    movb %al, (%rdi)        # store 00
+    inc %rdi
+    loop loop_clear
+
+    ret
+
+.section .note.GNU-stack,"",@progbits
     xor %eax, %eax  
 
 reset:
