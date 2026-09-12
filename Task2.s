ea41212f99dd71c808875a15ca5dd740b849f1d7
Kyr1e-1rving <michael.egharevba001@gmail.com>
2026-09-11 23:42:12 -0400

    Refactor fill_ram function

--- Task2.s
+++ Task2.s
@@ -1,20 +1,24 @@
 .section .bss
 .global ram
-.lcomm ram, 0x100
-.section .text
-.global reserve
+.lcomm ram, 256
 
-reserve:
-    lea ram(%rip), %rdi     
-    add $0x50, %rdi         
-    mov $9, %ecx           
-    mov $0xFF, %al        
-    #ret(no ret before loop)
-    
-move:
-    movb %al, (%rdi)   
-    inc %rdi       
-    loop move     
+.section .text
+.global fill_ram
+fill_ram:
+    # Indirect addressing: use pointer register
+
+    lea ram(%rip), %rdi     # rdi = address of ram[0]
+    add $0x50, %rdi         # address of ram[0x50]
+
+    mov $9, %ecx            # (50H to 58H)
+    mov $0xFF, %al         
+
+loop_start:
+    movb %al, (%rdi)        # store FF at address in rdi
+    inc %rdi                # move to next memory location
+    loop loop_start         # decrement ecx iff 0 else{ repeats loop
+
     ret
+    #ret(no ret before loop)
 
 .section .note.GNU-stack,"",@progbits
