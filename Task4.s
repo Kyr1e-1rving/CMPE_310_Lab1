20125cc2cf6b84879e88627ef07385651a8d4c11
Kyr1e-1rving <michael.egharevba001@gmail.com>
2026-09-12 00:03:03 -0400

    Implement fill_ram to compute sum and store in RAM
    Added fill_ram function to compute and store sum in RAM.

--- Task4.s
+++ Task4.s
@@ -2,7 +2,31 @@
 .global ram
 .lcomm ram, 0x100
 .section .text
-.global reserve
+.global reserve.section .bss
+.global ram
+.lcomm ram, 256
+
+.section .text
+.global fill_ram
+fill_ram:
+    # Compute 1+2+...+10 and store result at RAM[50H]
+
+    xor %eax, %eax          # AL = sum = 0
+    mov $1, %bl             # BL = i = 1
+    mov $10, %ecx           # 10 loops
+
+sum_loop:
+    add %bl, %al            # sum += i
+    inc %bl                 # i++
+    loop sum_loop
+
+    # store sum into ram[0x50]
+    lea ram(%rip), %rdi
+    movb %al, 0x50(%rdi)
+
+    ret
+
+.section .note.GNU-stack,"",@progbits
 reserve:
     xor %eax, %eax         
     mov $1, %bl            
